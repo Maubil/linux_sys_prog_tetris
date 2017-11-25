89bcca3d2b2369a306181fde000713d1309ef443
Maurice Billmann <maurice.billmann@gmail.com>
2017-11-25 22:52:35 +0100

    add server source

---
+++ src/server.c
@@ -0,0 +1,258 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define LISTEN_BACKLOG 50
+
+
+static void print_usage(const char *prog_name);
+static SOCK init_connection(const char *server_ip, const char *server_port);
+static int read_server(SOCKET sock, char *buffer);
+static void write_server(SOCKET sock, const char *buffer);
+
+int main(int argc, char *argv[])
+{
+    char c = 0;
+    char *server_ip = "127.0.0.1";
+    char *server_port = "30001";
+    SOCKET sock = 0;
+
+    /* tetris_server [-p \<port\>] [-h]
+    The value of the -p option specifies the TCP port of the socket that server shall listen to. 
+    The server shall listen on all possible interfaces in case there are more than one. 
+    If the IP is missing 127.0.0.1 shall be used on default. Also, select a suitable default port number (anything but 31457…​ why?).
+
+    If the -h option is given either program should display a sensible usage message explaining the syntax and exit afterwards. 
+    An illegal IP or port address should also trigger the display of the usage message and a termination. 
+    Both application should return with 0 on success or 1 if there are any errors (including but not limited to illegal IPs/ports). 
+    Use getopt(3) to handle the arguments.
+    */
+
+    while ( (c = getopt(argc, argv, "hp:")) != -1 ) {
+        switch ( c ) {
+            case 'p':
+                /* user passed server port */
+                int check_port = atoi(optarg);
+                /* only allow user ranged ports and not terinet port */
+                if(check_port >= 65535 || check_port <= 1024 || check_port == 31457)
+                {
+                    print_usage(argv[0]);
+                    return 1;
+                }
+                break;
+
+            case 'h':
+                /* print usage and exit without failure */
+                print_usage(argv[0]);
+                return 0;
+
+            case '?':
+                /* wrong usagem print usage and return with an error */
+                print_usage(argv[0]);
+                return 1;
+        }
+    }
+
+    /* we are ready to start the game */
+    do
+    {
+
+    }
+    sock = init_connection(server_ip, server_port);
+
+    char server_data[BUF_SIZE] = "";
+    while(server_data[0] != 'q')
+    {
+        printf()
+    }
+
+    closesocket(sock);
+    return 0;
+}
+
+/*! \brief Initialize the connection to the remote server.
+    \param server_ip    server IP string formatted.
+    \param server_port  server port string formatted.
+    \return socket number
+    \other  inspired by the getaddrinfo manual example.
+*/
+static SOCK init_connection(const char *server_port)
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    s = getaddrinfo(NULL, server_port, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+        Try each address until we successfully bind(2).
+        If socket(2) (or bind(2)) fails, we (close the socket
+        and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) 
+    {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+            break;                  /* Success */
+
+        close(sfd);
+    }
+
+    if (rp == NULL) 
+    {               /* No address succeeded */
+        fprintf(stderr, "Could not bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sfd, LISTEN_BACKLOG) == -1)
+    {
+        fprintf(stderr, "Could not listen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    peer_addr_size = sizeof(struct sockaddr_un);
+    cfd = accept(sfd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+    if (cfd == -1)
+    {
+        fprintf(stderr, "Could not accept\n");
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+    return sfd;
+}
+
+static void print_usage(const char *prog_name)
+{
+    fprintf(stderr, "Usage: %s [-p <port>] [-h]\n"
+                    "Options:\n"
+                    "  -p <port>\t\tPort to use.\n"
+                    "  -h\t\t\t\tPrint help and exit.\n",
+                    prog_name);
+}
+
+static int read_server(SOCKET sock, char *buffer)
+{
+   int n = 0;
+
+   if((n = recv(sock, buffer, BUF_SIZE - 1, 0)) < 0)
+   {
+      perror("recv()");
+      exit(errno);
+   }
+
+   buffer[n] = 0;
+
+   return n;
+}
+
+static void write_server(SOCKET sock, const char *buffer)
+{
+   if(send(sock, buffer, strlen(buffer), 0) < 0)
+   {
+      perror("send()");
+      exit(errno);
+   }
+}
+
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s;
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_len;
+    ssize_t nread;
+    char buf[BUF_SIZE];
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    s = getaddrinfo(NULL, argv[1], &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+        Try each address until we successfully bind(2).
+        If socket(2) (or bind(2)) fails, we (close the socket
+        and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) 
+    {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+            break;                  /* Success */
+
+        close(sfd);
+    }
+
+    if (rp == NULL) 
+    {               /* No address succeeded */
+        fprintf(stderr, "Could not bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+    /* Read datagrams and echo them back to sender */
+
+    for (;;) {
+        peer_addr_len = sizeof(struct sockaddr_storage);
+        nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+                (struct sockaddr *) &peer_addr, &peer_addr_len);
+        if (nread == -1)
+            continue;               /* Ignore failed request */
+
+        char host[NI_MAXHOST], service[NI_MAXSERV];
+
+        s = getnameinfo((struct sockaddr *) &peer_addr,
+                        peer_addr_len, host, NI_MAXHOST,
+                        service, NI_MAXSERV, NI_NUMERICSERV);
+        if (s == 0)
+            printf("Received %zd bytes from %s:%s\n",
+                    nread, host, service);
+        else
+            fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
+
+        if (sendto(sfd, buf, nread, 0,
+                    (struct sockaddr *) &peer_addr,
+                    peer_addr_len) != nread)
+            fprintf(stderr, "Error sending response\n");
+    }
+}

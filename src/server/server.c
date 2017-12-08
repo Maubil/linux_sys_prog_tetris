59204615c0cbf3b93bf50731b848c17ca9470b4e
Billmann <Maurice.Billmann@zuehlke.com>
2017-12-08 18:25:04 +0100

    add game handling and drafted socket handling

--- src/server/server.c
+++ src/server/server.c
@@ -2,23 +2,28 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <netdb.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <termios.h>
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
+#include <pthread.h>
+#include "../game.h"
 
-#define BUF_SIZE 255
+#define BUF_SIZE (255)
+#define INVALID_CLIENT_ID (-1)
+#define DELAY_MS (10)
 
 typedef int SOCKET;
 
 static void print_usage(const char *prog_name);
 static int read_server(SOCKET sock, char *buffer);
-static void write_server(SOCKET sock, const char *buffer);
+static void send_data(SOCKET sock, const char *buffer);
 static int child_process(SOCKET sock);
 static void finish(int sig);
+static int get_client_id(void);
 
 int main(int argc, char *argv[])
 {
@@ -130,23 +135,97 @@ static int read_server(SOCKET sock, char *buffer)
    return n;
 }
 
-static void write_server(SOCKET sock, const char *buffer)
+static int get_client_id(void)
 {
+    static bool client_in_use[CLIENTS_MAX] = {0};
+    static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    int next_client_id = INVALID_CLIENT_ID;
+
+    pthread_mutex_lock(&mutex);
+    for(int i = 0; i < CLIENTS_MAX; i++)
+    {
+        if(!client_in_use[i])
+        {
+            next_client_id = i;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+
+    return next_client_id;
+}
+#if 0
+static void send_data(SOCKET sock, struct game_state *gs)
+{
+    void *data = calloc(1, sizeof(struct game_state) + FIELD_SIZE);
+
+struct game_state {
+    enum tet_phase phase; /* The current phase/state of the game */
+    unsigned int points;  /* The current game points a player got */
+    unsigned int level;   /* The current level of the game */
+    unsigned int togo;    /* The number of lines to clear till next level */
+    /* A point to the two-dimensional array representing the play field */
+    char (*field)[FIELD_HEIGHT][FIELD_WIDTH];
+};
+
+    data[0] = htonl(gs->phase);
+    data[0] = htonl(gs->phase);
+
     if(send(sock, buffer, strlen(buffer), 0) < 0)
     {
+        free(data);
         perror("send()");
         exit(errno);
     }
+    free(data);
 }
-
+#endif
 static int child_process(SOCKET sock)
 {
-    char buf[100];
-    static int counter = 0;
+    enum tet_input recvdata = TET_VOID;
 
-    printf("Child just forked!\n");
-    snprintf(buf, sizeof buf, "hi %d", counter++);
-    send(sock, buf, strlen(buf), 0);
+    int client_id = get_client_id();
+    if (client_id == INVALID_CLIENT_ID)
+    {
+        // ERROR
+        finish(0);
+    }
+
+    printf("Child just forked! Starting client %d\n", client_id);
+
+    init_game(client_id);
+
+    while(1)
+    {
+        struct game_state *gs = NULL;
+
+        //recv(sock, &recvdata, sizeof(struct tet_input), 0);
+
+        /* Move current block one column left or right */
+        gs = handle_input(client_id, recvdata);
+        nanosleep(&(struct timespec){0, DELAY_MS*1000*1000}, NULL);
+
+        /* Do up to 200% of substeps required for one full time step.
+         * Thus for every step right/left above, do up to two steps down. */
+        for (size_t i = 0; i < (2 * STEP_TIME_INIT/STEP_TIME_GRANULARITY); i++) 
+        {
+            gs = handle_substep(client_id);
+        }
+        if (gs->phase == TET_LOSE || gs->phase == TET_WIN) 
+        {
+            fprintf(stderr,
+                    "Player %s with %u points in level %u.\n",
+                    gs->phase == TET_WIN ? "wins" : "loses",
+                    gs->points,
+                    gs->level);
+                exit(1);
+        }
+
+        /* serialize data */
+
+        nanosleep(&(struct timespec){0, DELAY_MS*1000*1000}, NULL);
+    }
+    
     return 0;
 }
 
